d9cefd40b5421245b80ea49ed262aa810b4b8a23
Yolaafifah <yolasabrina000@gmail.com>
2021-11-02 22:09:09 +0700

    update database

--- config/database.php
+++ config/database.php
@@ -2,13 +2,13 @@
 
 // file koneksi ke database
 
-$server = "localhost"; // nama server yang temen-temen gunain. Ini default value kalau pake XAMPP.
-$username = "root";
-$password = ""; // sesuaikan dengan pasaword temen-temen :)
+$server = "10.0.0.31"; // nama server yang temen-temen gunain. Ini default value kalau pake XAMPP.
+$username = "admin";
+$password = "Admin1,02/okt,21"; // sesuaikan dengan pasaword temen-temen :)
 $database = "incgrup"; // nama database yang digunakan.
 
 // koneksi ke database
-@$db = mysqli_connect($server, $username, $password, $database);
+$db = mysqli_connect($server, $username, $password, $database);
 
 // terkoneksi atau belum ?
 if (!$db) {
